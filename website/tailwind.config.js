47b11a09c0681e8991b239400347ccf38b8d6837
gergelyvizvari <vizvari.gergely@gmail.com>
2023-03-31 12:26:00 +0100

    wip

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,3 +1,29 @@
-import config from './../ui/shared/tailwind.config.js'
+import daisyui from "daisyui"
+import typography from "@tailwindcss/typography"
 
-export default config;
+export default {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            maxWidth: 'inherited', // add required value here          
+          },
+        }
+      }),
+    },
+  },
+  plugins: [
+    daisyui,
+    typography,
+  ],
+  daisyui: {
+    themes: [
+      'light',
+      'dark',
+    ],
+  }
+}
